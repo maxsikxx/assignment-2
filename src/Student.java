117a8a4a4936fcf998392ea83fe684a15448957c
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:00:48 +0600

    created variables

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,8 @@
-public class Student {
+public class Student extends Person {
+    private double gpa;
+    private static final double step = 36660.00;
+
+    public Student() {
+        super();
+    }
 }
