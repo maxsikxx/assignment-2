00e422cc76e9c43b3bb704528b476b16291e488d
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:38:00 +0600

    used inheritance and defined a superclass, used toString()

--- src/Person.java
+++ src/Person.java
@@ -14,4 +14,38 @@ public class Person implements Payable, Comparable<Person> {
         this.surname = surname;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return "Студент";
+    }
+
+    @Override
+    public String toString() {
+        return getPosition() + ": " + id + ". " + name + " " + surname;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Double.compare(other.getPaymentAmount(), getPaymentAmount());
+    }
 }
