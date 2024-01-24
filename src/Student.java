aad02107ba6799c22867d0cc6c66b7460d258c9e
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:03:11 +0600

    used constructor

--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,11 @@ public class Student extends Person {
     public Student() {
         super();
     }
+
+    public Student(String name, String surname, double gpa) {
+        super(name, surname);
+        this.gpa = gpa;
+    }
+
+
 }
