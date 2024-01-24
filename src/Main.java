be3ba427f8185188ea60948d73b04f3dcadeed92
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:27:45 +0600

    the necessary classes were created, I entered data into the array, sorted the array and extracted the array elements

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Employee("Tannur", "Talasbay", "Куратор", 100000));
+        people.add(new Student("Asel", "Kenzhetay", 2.3));
+        people.add(new Student("Edil", "Myrzabek", 3.7));
+        people.add(new Employee("Maksat", "Karzhaubaev", "Куратор", 250000.00));
+        Collections.sort(people);
+        Collections.reverse(people);
+        printData(people);
+    }
 
+    public static void printData(Iterable<Person> people) {
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
 }
