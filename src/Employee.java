a4fe2c25c02ed8b2e1f823e8f5a02e345c88e78e
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:41:50 +0600

    created getters, wrote other methods, use toString(), created setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,41 @@
-public class Employee {
+public class Employee extends Person {
+    private String position;
+    private double salary;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String name, String surname, String position, double salary) {
+        super(name, surname);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " зарабатывает " + salary + " тенге";
+    }
 }
