0e05c824424308be5301b36fb6e53c6af3098afc
maxsikxx <231324@astanait.edu.kz>
2024-01-25 01:47:40 +0600

    created getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -6,5 +6,18 @@ public class Employee extends Person {
         super();
     }
 
+    public Employee(String name, String surname, String position, double salary) {
+        super(name, surname);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
 }
