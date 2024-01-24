f8851ced9ca62a73fd78e04f0a134019b342a41c
maxsikxx <231324@astanait.edu.kz>
2024-01-25 00:42:42 +0600

    created variables, constructors

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,9 @@
-public class Person {
+public class Person implements Payable, Comparable<Person> {
+    private String name;
+    private String surname;
+    private final int id;
+    private static int counter = 1;
+
+
+
 }
